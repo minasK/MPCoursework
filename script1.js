0a3998ec10c0e4868aa63e93184d610d5065a92a
spoggos1 <91052397+spoggos1@users.noreply.github.com>
2023-07-01 23:27:35 +0300

    Update script1.js
    updated file's documentation.

--- script1.js
+++ script1.js
@@ -1,21 +1,22 @@
 
-  $(document).ready(function () {
+  $(document).ready(function () {/*this function adds items to the table when the user clicks "add movie", and also allows for sorting of the column movie name and genre by alphabetical order. */
+  // jQuery button click event to add a row
     // jQuery button click event to add a row
     $('#addBtn').on('click', function () {
     var movieList = [];
     var genreList = [];
-    var movieName = $('#movieNameInput').val();
-    var genre = $('#genreInput').val();
-    movieList.push(movieName);
-    genreList.push(genre);
+    var movieName = $('#movieNameInput').val(); /*set the value of the variable "moviename" to be equal to the value of the text input "#movieNameInput" */
+    var genre = $('#genreInput').val(); /*same for the value genre */
+    movieList.push(movieName); /*append the value of the "movieName" variable into the javascript array defined as "movieList" */
+    genreList.push(genre); /*same for the value genre */
     
       
-  if (movieName == '' || genre == '') {
+  if (movieName == '' || genre == '') { *disallow blank entries when clicking "add movie" to protect the table from empty rows */
     alert('Please enter a movie name and genre.');
     return; 
   }
 
-    
+    /*lines 18-24 append the values found in the "movieList" and "movieGenre" arrays as table data in the table body */
     $('#tbody').append(`<tr>
       <td>${movieList}</td>
       <td>${genreList}</td>
@@ -24,12 +25,12 @@
       </td>
     </tr>`);
 
-      // clear input fields
+      // set the values of the text inputs "movienameInput" and "genreInput" to the empty string so that the user can easily add their next movie of choice to the table
       $('#movieNameInput').val('');
       $('#genreInput').val('');
     });
 
-    // function to remove row closest 
+    // this function removes the closest tablerow to the remove button --this work fine because each row has its own remove button.
     $('#tbody').on('click', '.remove', function () {
       $(this).closest('tr').remove();
     });
